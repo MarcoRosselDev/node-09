fd6b8528e83ef13bfeb8bdab67242dd7f42947af
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-02 20:54:54 -0400

    cv | 05

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const cssBtn = document.querySelector('.cssClass');
 
 cssBtn.addEventListener('click', function (e) {
     e.preventDefault()
-    const bodyClass = this.parentNode.parentNode.className
+    const bodyClass = this.parentNode.parentNode.className;
+    const navBarClass = this.parentNode.className;
     if (bodyClass == 'body-light') {
         this.parentNode.parentNode.className = 'body-dark'
+        this.parentNode.className = 'nav-bar-dark'
     } else{
         this.parentNode.parentNode.className = 'body-light'
+        this.parentNode.className = 'nav-bar-light'
     }
     console.log(this.parentNode.parentNode.className);
 })
