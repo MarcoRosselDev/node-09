654e5e051a2a57408fc5e2044538e07531efc0ad
Marco Rossel <andresmarcorossel@gmail.com>
2025-02-26 22:44:22 -0300

    table

---
+++ table.js
@@ -0,0 +1,17 @@
+const name = 'Marco';
+const age = 31;
+const country = 'Chile';
+
+console.table({name, age, country})
+/* 
+┌─────────┬─────────┐
+│ (index) │ Values  │
+├─────────┼─────────┤
+│ name    │ 'Marco' │
+│ age     │ 31      │
+│ country │ 'Chile' │
+└─────────┴─────────┘
+ */
+//console.log({nombre: 'marco', age:1, country:'as'});
+
+//console.log('aklsjdf');
