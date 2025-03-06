5f883d90ddea6580ea1392ebee34da9ca1f41e28
Marco Rossel <andresmarcorossel@gmail.com>
2025-03-06 15:23:31 -0300

    pg conection successful

---
+++ test.js
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+console.log(process.env.NAME_PORT);
+
+import pgpPromise from 'pg-promise';
+const pgp = pgpPromise({});
+const {PASS_PSQL, PORT_PSQL, DATABASE, NAME_PORT, HOST} = process.env;
+
+//const db = pgp(`${DATABASE}://${NAME_PORT}:${PASS_PSQL}@host:${PORT_PSQL}/database`);
+const db = pgp(`${DATABASE}://${NAME_PORT}:${PASS_PSQL}@${HOST}:${PORT_PSQL}/${DATABASE}`);
+
+console.log(PASS_PSQL);
+
+//const db = pgp('postgres://postgres:pol.123.pol@localhost:5001/postgres');
+
+try {
+  const customers = await db.any('select * from categories;');
+  console.log(customers);
+} catch (error) {
+  console.log(error);
+}
+
+/* db.any('SELECT * FROM categories')
+  .then((data) => {
+    console.log('DATA:', data)
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+  }) */
